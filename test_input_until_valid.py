1c8a33d908b742e74d3f2cab23ec0ec8d786f49e
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2023-02-06 11:37:29 +0100

    Create test_input_until_valid.py
    Started this code today.

---
+++ test_input_until_valid.py
@@ -0,0 +1,18 @@
+"""
+Filename: test_input_until_valid.py
+Purpose: To test working within git and github
+Arthor: d_abah
+"""
+
+while True:
+  try:
+    my_level = input('A single line of string is accepted here.')
+    if my_level.lower() == 'yes':
+      my_level = True
+      bread; 
+    else:
+      my_level = False
+    exception:
+      continue
+print(my_level)
+    
